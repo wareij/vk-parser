37c8a485043785797dddaa6cfae3daad32d0c67d
Buzuev Aleksandr <baa9763@mail.ru>
2021-10-25 22:40:50 +0400

    vk-parser_v1

---
+++ groups.py
@@ -0,0 +1,56 @@
+import requests
+import time
+from kek import*
+
+
+# Задаем колличество постов, которые проверит скрипт в каждой группе
+# (Посты будут проверяться начиная с самых свежих. Максимальное число - 100)
+POSTS = 20
+
+
+# Получам список айдишников и названий сообществ заданного пользователя
+def get_groups():
+    g_values = {}
+    groups = (requests.get(S_URL + 'groups.get', params={'extended': 1, 'user_id': USER_ID,'access_token': ACCESS_TOKEN, 'v': 5.131})).json()    
+    groups = groups['response']['items']
+    for i in groups:
+        g_values[i['id']] = i['screen_name']
+
+    return g_values
+
+# Получаем списки лайков заданного пользователя по сообществам
+def get_likes(g_values):
+    change_id = ('-' + str(g_values[0]))
+    likes = []
+    try:
+        for i in range(POSTS):
+            post = (requests.get(S_URL + 'wall.get', params={'offset': i, 'owner_id': change_id, 'count': 1,
+                                                             'access_token': ACCESS_TOKEN, 'v': 5.131})).json()
+            post_id = post['response']['items'][0]['id'] 
+            time.sleep(0.2)
+
+            like = (requests.get(S_URL + 'likes.isLiked', params={'user_id': USER_ID, 'type': 'post', 'owner_id': change_id,
+                                                                 'item_id': post_id, 'access_token': ACCESS_TOKEN, 'v': 5.131})).json()
+            like = like['response']['liked']
+
+            if like == 1:
+                likes.append(f'https://vk.com/{g_values[1]}?w=wall{change_id}_{post_id}')
+            else:
+                likes.append(like)
+            time.sleep(0.2)
+    except:
+        None
+        
+    return likes    
+
+def calc():
+    for i in get_groups().items():        
+        print(i)
+        print(get_likes(i))
+        time.sleep(0.2)        
+
+def main():    
+    calc()
+   
+if __name__ == '__main__':
+    main()
---
+++ kek.py
@@ -0,0 +1,3 @@
+ACCESS_TOKEN = '1ad54629415203cabd534e7954fc98dc7a0d7c08415ec71ca3e23af40a3def21a363cc9f6cdcdeae4192d'
+USER_ID = '486660548'
+S_URL = 'https://api.vk.com/method/'
